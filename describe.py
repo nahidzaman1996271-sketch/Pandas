5f2058d7f94848b37a5f30b38a9aed3a84066417
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-20 01:08:28 +0600

    and today i am so cooked and my body can't keep up

---
+++ describe.py
@@ -0,0 +1,14 @@
+# step-1 sample data frame
+
+import pandas as pd
+
+data = {
+    "Name": ['Nahid','Ibn','Zaman','Ikfa','Symooon'],
+    "Age": [10,20,30,40,50],
+    "Salary": [900000,5000,6000,40000,100000],
+    "Performance Score": [20,50,60,40,15]
+}
+
+df = pd.DataFrame(data)
+print("Sample Dataframe")
+print(df)
