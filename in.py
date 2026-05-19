1148a68cac1862364a4f32f86d1752df396e9f5f
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-20 00:29:18 +0600

    learnt info

--- in.py
+++ in.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-df = pd.read_excel("D:\Pandas\sample_data.xlsx")
+data = {
+    "Name": ['Nahid','Mashuk','Pranjol','Mahmuda','Sazon'],
+    "Age" : [10,20,30,25,80],
+    "City": ['Dhaka','Cumilla','Barishal','Pabna','Tangail']
+}
+
+df = pd.DataFrame(data)
 
 print('Displaying the into of data set')
 print(df.info())
