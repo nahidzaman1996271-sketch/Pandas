d76b50581cd27f728a472687a7dbc55e9dca7e0c
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-20 00:18:52 +0600

    start learning pandas for today

--- rows.py
+++ rows.py
@@ -1,1 +1,12 @@
 # head() tail()
+# head() 5
+# tail(n) 5
+
+import pandas as pd
+
+df = pd.read_excel("D:\Pandas\sample_data.xlsx")
+print('Display 10 rows of first')
+print(df.head(10))
+
+print('Display 10 rows of last')
+print(df.tail(10))
