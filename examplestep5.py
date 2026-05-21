ac7ac46f9206969f234cc2753baf5dc029090108
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-22 00:18:38 +0600

    learnt how to select single column and multiple column

---
+++ examplestep5.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = {
+    "Name": ['Nahid','Ibn','Zaman','Ikfa','Symooon'],
+    "Age": [10,20,30,40,50],
+    "Salary": [900000,5000,6000,40000,100000],
+    "Performance Score": [20,50,60,40,15]
+}
+
+df = pd.DataFrame(data)
+
+# display the dataframe
+print("Sample Dataframe")
+print(df)
+print("Names (Single column return series)")
+name = df['Name']
+print(name)
+
+
+# selecting multiple columns
+subset = df[["Name","Salary"]]
+print("\nSubset with Name and Salary")
+print(subset)
