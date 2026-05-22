2fa5d8a8e2cf475a07904dd4b514fdb226533f51
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-22 23:18:10 +0600

    it is pretty awesome and it is supereasy

---
+++ Handling Missing Data/missing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "Name": ['Nahid',None,'Ibn','Zaman','Ikfa','Symooon'],
+    "Age": [10,None,20,30,40,50],
+    "Salary": [90000,None,5000,6000,40000,10000],
+    "Performance Score": [20,None,50,60,40,15]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+print(df.isnull())
