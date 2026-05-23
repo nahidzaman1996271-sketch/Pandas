40542275c3018ba3c4f0fb1cb185b1c21b02eb9f
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-23 23:17:00 +0600

    and here we learn how to group the data

---
+++ Sorting and Aggregation/grouping.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "Name":['Nahid','Ibn','Zaman','Sagor','Sumon'],
+    "Age": [28,44,35,56,19],
+    "Salary": [50000,40000,60000,55000,48000]
+}
+
+df = pd.DataFrame(data)
+grouped = df.groupby("Age")["Salary"].sum()
+print(grouped)
